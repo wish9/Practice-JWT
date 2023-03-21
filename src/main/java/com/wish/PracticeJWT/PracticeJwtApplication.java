fe0f8703c5ac66be5f3523c7bccc5fe62931cc9b
wish9 <wjwee9@gmail.com>
2023-03-21 15:48:56 +0900

    .

---
+++ src/main/java/com/wish/PracticeJWT/PracticeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.wish.PracticeJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeJwtApplication.class, args);
+	}
+
+}
